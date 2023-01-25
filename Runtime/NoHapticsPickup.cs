199b29d319c6de4af1a30843e523aeba2ee66b60
Atodeyaru <56915208+atodeyaru@users.noreply.github.com>
2023-01-25 21:44:05 +0900

    initial commit

---
+++ Runtime/NoHapticsPickup.cs
@@ -0,0 +1,86 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon.Common;
+
+namespace Atodeyaru.Udon
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Continuous)]
+    public class NoHapticsPickup : UdonSharpBehaviour
+    {
+        [SerializeField] private Transform _exactGrip;
+        [SerializeField] private bool _AutoHold;
+
+        private bool _picking;
+        private bool _left;
+        private Vector3 _positionOffset;
+        private Quaternion _rotationOffset;
+
+        void Start()
+        {
+        }
+
+        private void Update()
+        {
+            if (_picking)
+            {
+                var hand = Networking.LocalPlayer.GetTrackingData(_left ? VRCPlayerApi.TrackingDataType.LeftHand : VRCPlayerApi.TrackingDataType.RightHand);
+                this.transform.position = hand.position + hand.rotation * _positionOffset;
+                this.transform.rotation = hand.rotation * _rotationOffset;
+            }
+        }
+
+        public override void OnOwnershipTransferred(VRCPlayerApi player)
+        {
+            Drop();
+        }
+
+        public override void InputDrop(bool value, UdonInputEventArgs args)
+        {
+            if (args.handType == (_left ? HandType.LEFT : HandType.RIGHT) && value)
+            {
+                Drop();
+            }
+        }
+
+        public override void InputGrab(bool value, UdonInputEventArgs args)
+        {
+            if (args.handType == (_left ? HandType.LEFT : HandType.RIGHT) && !value && !_AutoHold)
+            {
+                Drop();
+            }
+        }
+
+        public void Grab(bool left)
+        {
+            TakeOwnership();
+            _picking = true;
+            _left = left;
+            var hand = Networking.LocalPlayer.GetTrackingData(left ? VRCPlayerApi.TrackingDataType.LeftHand : VRCPlayerApi.TrackingDataType.RightHand);
+            if (_exactGrip != null)
+            {
+                _positionOffset = Quaternion.Inverse(_exactGrip.rotation) * (this.transform.position - _exactGrip.position);
+                _rotationOffset = Quaternion.Inverse(_exactGrip.rotation) * this.transform.rotation;
+            }
+            else
+            {
+                _positionOffset = Quaternion.Inverse(hand.rotation) * (this.transform.position - hand.position);
+                _rotationOffset = Quaternion.Inverse(hand.rotation) * this.transform.rotation;
+            }
+        }
+
+        private void Drop()
+        {
+            _picking = false;
+        }
+
+        private void TakeOwnership()
+        {
+            if (VRCPlayerApi.GetPlayerCount() > 1 && !Networking.IsOwner(this.gameObject))
+            {
+                Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+            }
+        }
+    }
+}
---
+++ Runtime/PickupHand.cs
@@ -0,0 +1,68 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon.Common;
+
+namespace Atodeyaru.Udon
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class PickupHand : UdonSharpBehaviour
+    {
+        [SerializeField] private LayerMask _layerMask = int.MaxValue;
+        [SerializeField] private float _pickupRadius = 0.05f;
+        [SerializeField] private float _desktopPickupRadius = 1.0f;
+
+        private bool _isUserInVR;
+
+        void Start()
+        {
+            _isUserInVR = Networking.LocalPlayer.IsUserInVR();
+        }
+
+        public override void InputGrab(bool value, UdonInputEventArgs args)
+        {
+            if (value)
+            {
+                if (_isUserInVR)
+                {
+                    var left = args.handType == HandType.LEFT;
+                    var hand = Networking.LocalPlayer.GetTrackingData(left ? VRCPlayerApi.TrackingDataType.LeftHand : VRCPlayerApi.TrackingDataType.RightHand);
+                    var colliders = Physics.OverlapSphere(hand.position, _pickupRadius, _layerMask, QueryTriggerInteraction.Collide);
+
+                    NoHapticsPickup pickup;
+                    foreach (var collider in colliders)
+                    {
+                        if (collider == null) continue;
+
+                        pickup = collider.GetComponent<NoHapticsPickup>();
+                        if (pickup != null)
+                        {
+                            pickup.Grab(left);
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    var head = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+                    
+                    var hits = Physics.RaycastAll(head.position, head.rotation * Vector3.forward, _desktopPickupRadius, _layerMask, QueryTriggerInteraction.Collide);
+
+                    NoHapticsPickup pickup;
+                    foreach (var hit in hits)
+                    {
+                        if (hit.collider == null) continue;
+
+                        pickup = hit.collider.gameObject.GetComponent<NoHapticsPickup>();
+                        if (pickup != null)
+                        {
+                            pickup.Grab(false);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
